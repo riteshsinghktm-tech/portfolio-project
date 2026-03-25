3834a20ee0ee770812e359091a6802b42387034d
Ritesh Singh <riteshsinghktm@gmail.com>
2026-03-25 21:22:43 +0530

    final animated portfolio

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "YOUR_PASSWORD",
+    password: "root123",
     database: "portfolio_db"
 });
 
